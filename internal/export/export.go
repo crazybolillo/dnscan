652aaa2781086905de38c16bae51c4c02f1e9afb
CrazyBolillo <antonio@zoftko.com>
2024-05-10 14:19:36 -0600

    feat: implement exporter

--- internal/export/export.go
+++ internal/export/export.go
@@ -1,1 +1,24 @@
 package export
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/crazybolillo/dnscan/internal/scan"
+)
+
+func Start(ctx context.Context, input <-chan scan.Result, writer io.Writer) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case result, ok := <-input:
+			if !ok {
+				return
+			}
+			msg := fmt.Sprintf("%s: %v\n", result.Host, result.IPs)
+			writer.Write([]byte(msg))
+		}
+	}
+}
